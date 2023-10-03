95703f18c856d51d2a2f6d8016547920e65680a0
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-10-03 11:58:25 +0300

    Display api request result in result.html

---
+++ src/main/java/BigB_Project/LocationFinder/Controlers/ApiController.java
@@ -0,0 +1,33 @@
+package BigB_Project.LocationFinder.Controlers;
+
+import BigB_Project.LocationFinder.Services.LocationFinderServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping(value = "/geocoding")
+public class ApiController {
+
+    @Autowired
+    LocationFinderServices locationFinderServices;
+
+    // http://localhost:8080/geocoding/home
+    @GetMapping(path = "/home")
+    public String displayHomePage(){
+        return "home";
+    }
+
+    // http://localhost:8080/geocoding/coordinates
+    @GetMapping(value = "/coordinates")
+    public String getCoordinates(Model model) throws IOException, InterruptedException {
+
+        String coordinates = locationFinderServices.connectToApi();
+        model.addAttribute("coordinates", coordinates);
+        return "result";
+    }
+}
