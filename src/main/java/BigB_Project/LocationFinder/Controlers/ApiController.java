febbdf0dfa59fed8e30523ff16aefc6b85bded6c
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-11-13 14:57:24 +0200

    Update mapview, hide API KEY

--- src/main/java/BigB_Project/LocationFinder/Controlers/ApiController.java
+++ src/main/java/BigB_Project/LocationFinder/Controlers/ApiController.java
@@ -2,6 +2,7 @@ package BigB_Project.LocationFinder.Controlers;
 
 import BigB_Project.LocationFinder.Services.LocationFinderServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,17 @@ import java.io.IOException;
 @Controller
 @RequestMapping(value = "/geocoding")
 public class ApiController {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+//    http://localhost:8080/geocoding/map
+    @GetMapping("/map")
+    public String mapPage(Model model) {
+        model.addAttribute("apiKey", apiKey);
+        return "mapviewoption"; // Mano sprendimo būdas, kaip dar panaudoti directions API
+//        return "mapview";
+    }
 
     @Autowired
     LocationFinderServices locationFinderServices;
